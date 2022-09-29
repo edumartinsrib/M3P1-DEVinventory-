c7fb9cf912156fa56d9233341ec1e15194c945e2
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-09-29 19:55:25 -0300

    test: 🎸 tests post user

--- tests/users/test_post_user.py
+++ tests/users/test_post_user.py
@@ -13,7 +13,8 @@ payload = {
         "number_street": 210
 }
 
-keys_requireds = ["city_id", "gender_id", "role_id", "name", "email", "password"]
+keys_requireds = ["city_id", "gender_id", "role_id", "name", "email", "password", ]
+keys_not_requireds = ["cep", "street", "district", "number_street", 'phone']
 
 def test_post_user_success(client, logged_in_client):
     """Test of the post user route with a valid token"""
@@ -29,6 +30,7 @@ def test_post_user_with_email_already_exists(client, logged_in_client):
     response = client.post("/user/", headers=headers, json=payload)
     payload_test = payload.copy()
     payload_test["email"] = "joao@email.com"
+    
     if response.status_code == 400 and response.json:
         assert response.json["error"] == "{'email': ['Email já registrado']}"
 
@@ -46,12 +48,23 @@ def test_post_with_invalid_payload(client, logged_in_client):
         payload_test = payload.copy()
         payload_test.pop(key)
         response = client.post("/user/", headers=headers, json=payload_test)
-    
+
         if response.status_code == 400 and response.json:
             assert response.json["error"] == f"{{'{key}': ['{key} é obrigatório.']}}"
         else:
             assert False
-            
+
+def test_post_success_without_not_requireds_fields(client, logged_in_client):
+    """Test of the post user route with a valid token"""
+    headers = {"Authorization": f"Bearer {logged_in_client}"}
+    payload_test = payload.copy()
+    for key in keys_not_requireds:
+        payload_test.pop(key)
+        payload_test["email"] = f"email{key}@email.com"
+    response = client.post("/user/", headers=headers, json=payload_test)
+        
+    assert response.status_code == 201
+
 def test_post_with_invalid_payload_password(client, logged_in_client):
     """Test of the post user route with an invalid payload - password"""
     headers = {"Authorization": f"Bearer {logged_in_client}"}
@@ -63,3 +76,65 @@ def test_post_with_invalid_payload_password(client, logged_in_client):
         assert response.json["error"] == "{'password': ['Senha fraca, utilize letras maiúsculas, minúsculas, números e caracteres especiais']}"
     else:
         assert False
+        
+        
+def test_with_invalid_payload_telephone(client, logged_in_client):
+    """Test of the post user route with an invalid payload - telephone"""
+    headers = {"Authorization": f"Bearer {logged_in_client}"}
+    payload_test = payload.copy()
+    payload_test["phone"] = "4899999999"
+    response = client.post("/user/", headers=headers, json=payload_test)
+    
+    if response.status_code == 400 and response.json:
+        assert response.json["error"] == "{'phone': ['Telefone inválido']}"
+    else:
+        assert False
+        
+def test_with_invalid_payload_email(client, logged_in_client):
+    """Test of the post user route with an invalid payload - email"""
+    headers = {"Authorization": f"Bearer {logged_in_client}"}
+    payload_test = payload.copy()
+    payload_test["email"] = "joaoemail.com"
+    response = client.post("/user/", headers=headers, json=payload_test)
+    
+    if response.status_code == 400 and response.json:
+        assert response.json["error"] == "{'email': ['email inválido.']}"
+    else:
+        assert False
+        
+def test_with_invalid_payload_age(client, logged_in_client):
+    """Test of the post user route with an invalid payload - age"""
+    headers = {"Authorization": f"Bearer {logged_in_client}"}
+    payload_test = payload.copy()
+    payload_test["age"] = "2000/01/01"
+    response = client.post("/user/", headers=headers, json=payload_test)
+    
+    if response.status_code == 400 and response.json:
+        assert response.json['error'] == "time data '2000/01/01' does not match format '%d/%m/%Y'"
+    else:
+        assert False
+        
+def test_with_invalid_payload_city_id(client, logged_in_client):
+    """Test of the post user route with an invalid payload - city_id"""
+    headers = {"Authorization": f"Bearer {logged_in_client}"}
+    payload_test = payload.copy()
+    payload_test["city_id"] = "teste"
+    response = client.post("/user/", headers=headers, json=payload_test)
+    
+    if response.status_code == 400 and response.json:
+        assert response.json["error"] == "{'city_id': ['city_id inválido.']}"
+    else:
+        assert False
+
+def test_with_invalid_payload_gender_id(client, logged_in_client):
+    """Test of the post user route with an invalid payload"""
+    headers = {"Authorization": f"Bearer {logged_in_client}"}
+    payload_test = payload.copy()
+    payload_test["gender_id"] = 5
+    response = client.post("/user/", headers=headers, json=payload_test)
+    
+    if response.status_code == 400 and response.json:
+        assert response.json["error"] == "{'gender_id': ['Gênero não encontrado.']}"
+    else:
+        assert False
+        
