716cee30e51ff9ffb445bdf029614dc84de04e1c
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-10-01 23:44:28 -0300

    refact: enhancements test_post_inventory

--- tests/inventory/test_post_inventory.py
+++ tests/inventory/test_post_inventory.py
@@ -1,35 +1,50 @@
 from mocks import payload_configs_and_types, payload_just_values_and_keys
 
+configs = {
+    "url_base": "/inventory/",
+}
+
+
+def headers(logged_in_client):
+    return {"Authorization": f"Bearer {logged_in_client}"}
+
 
 def test_post_inventory_success_status_200(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     response = client.post(
-        f"/inventory/", json=payload_just_values_and_keys, headers=headers
+        configs["url_base"],
+        json=payload_just_values_and_keys,
+        headers=headers(logged_in_client),
     )
 
     if response.status_code == 201 and response.json:
         assert response.json["data"] != None
+    else: 
+        assert False 
 
 
 def test_post_inventory_fail_user_not_authorized_status_403(
     client, logged_in_client_with_user_read
 ):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client_with_user_read}"}
     response = client.post(
-        f"/inventory/", json=payload_just_values_and_keys, headers=headers
+        configs["url_base"],
+        json=payload_just_values_and_keys,
+        headers=headers(logged_in_client_with_user_read),
     )
 
     if response.status_code == 403 and response.json:
         assert "Você não tem permissão" in response.json["error"]
+    else: 
+        assert False   
 
 
 def test_post_inventory_fail_with_invalid_token(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}123"}
     response = client.post(
-        f"/inventory/", json=payload_just_values_and_keys, headers=headers
+        configs["url_base"],
+        json=payload_just_values_and_keys,
+        headers=headers(f"{logged_in_client}123"),
     )
 
     assert response.status_code == 403
@@ -37,9 +52,10 @@ def test_post_inventory_fail_with_invalid_token(client, logged_in_client):
 
 def test_post_inventory_fail_with_invalid_payload(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     response = client.post(
-        f"/inventory/", json={"invalid_key": "invalid_value"}, headers=headers
+        configs["url_base"],
+        json={"invalid_key": "invalid_value"},
+        headers=headers(logged_in_client),
     )
 
     assert response.status_code == 400
@@ -47,12 +63,13 @@ def test_post_inventory_fail_with_invalid_payload(client, logged_in_client):
 
 def test_post_inventory_fail_missing_fields_requireds(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     for payload_config in payload_configs_and_types:
         if payload_config["required"]:
             payload = payload_just_values_and_keys.copy()
             del payload[payload_config["key"]]
-            response = client.post(f"/inventory/", json=payload, headers=headers)
+            response = client.post(
+                configs["url_base"], json=payload, headers=headers(logged_in_client)
+            )
 
             if response.status_code == 400 and response.json:
                 assert "é obrigatório" in response.json["error"]
@@ -62,12 +79,13 @@ def test_post_inventory_fail_missing_fields_requireds(client, logged_in_client):
 
 def test_post_inventory_fail_with_type_wrong(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     for payload_config in payload_configs_and_types:
         if payload_config["type"] == int or payload_config["type"] == float:
             payload = payload_just_values_and_keys.copy()
             payload[payload_config["key"]] = "invalid_value"
-            response = client.post(f"/inventory/", json=payload, headers=headers)
+            response = client.post(
+                configs["url_base"], json=payload, headers=headers(logged_in_client)
+            )
 
             if response.status_code == 400 and response.json:
                 assert "inválido" in response.json["error"]
@@ -77,12 +95,13 @@ def test_post_inventory_fail_with_type_wrong(client, logged_in_client):
 
 def test_post_inventory_fail_with_product_code_unique(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     for payload_config in payload_configs_and_types:
         if payload_config["unique"]:
             payload = payload_just_values_and_keys.copy()
             payload[payload_config["key"]] = 1
-            response = client.post(f"/inventory/", json=payload, headers=headers)
+            response = client.post(
+                configs["url_base"], json=payload, headers=headers(logged_in_client)
+            )
 
             if response.status_code == 400 and response.json:
                 assert "já registrado" in response.json["error"]
@@ -92,12 +111,13 @@ def test_post_inventory_fail_with_product_code_unique(client, logged_in_client):
 
 def test_post_inventory_fail_with_invalid_value_null(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     for payload_config in payload_configs_and_types:
         if payload_config["key"] == "value":
             payload = payload_just_values_and_keys.copy()
             payload[payload_config["key"]] = 0
-            response = client.post(f"/inventory/", json=payload, headers=headers)
+            response = client.post(
+                configs["url_base"], json=payload, headers=headers(logged_in_client)
+            )
 
             if response.status_code == 400 and response.json:
                 assert "inválido" in response.json["error"]
@@ -107,12 +127,13 @@ def test_post_inventory_fail_with_invalid_value_null(client, logged_in_client):
 
 def test_post_inventory_fail_with_invalid_value_negative(client, logged_in_client):
     """Test of the user route with a valid token"""
-    headers = {"Authorization": f"Bearer {logged_in_client}"}
     for payload_config in payload_configs_and_types:
         if payload_config["key"] == "value":
             payload = payload_just_values_and_keys.copy()
             payload[payload_config["key"]] = -1
-            response = client.post(f"/inventory/", json=payload, headers=headers)
+            response = client.post(
+                configs["url_base"], json=payload, headers=headers(logged_in_client)
+            )
 
             if response.status_code == 400 and response.json:
                 assert "inválido" in response.json["error"]
